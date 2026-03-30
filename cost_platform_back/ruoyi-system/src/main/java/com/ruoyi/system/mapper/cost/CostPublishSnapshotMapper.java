8b68309e6ca52cff669af9df670533bd719edb9f
GitFhw <135774730+GitFhw@users.noreply.github.com>
2026-03-30 22:00:27 +0800

    feat: 增强发布治理能力

---
+++ cost_platform_back/ruoyi-system/src/main/java/com/ruoyi/system/mapper/cost/CostPublishSnapshotMapper.java
@@ -0,0 +1,13 @@
+package com.ruoyi.system.mapper.cost;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ruoyi.system.domain.cost.CostPublishSnapshot;
+
+/**
+ * 发布快照 Mapper
+ *
+ * @author codex
+ */
+public interface CostPublishSnapshotMapper extends BaseMapper<CostPublishSnapshot>
+{
+}
